3ea51fe69bd821d9319c4b9651b11581ea26f19d
Danial Eskandari <doneskandari@gmail.com>
2025-02-17 19:07:33 +0330

    feat: implement domain layer and core data modeling design

---
+++ internal/domain/models/realm.go
@@ -0,0 +1,28 @@
+// Copyright 2025 The Accession Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package models
+
+import "github.com/oklog/ulid/v2"
+
+// Multi-tenancy, Logical Partitioning
+type Realm struct {
+	identifier ulid.ULID
+}
+
+func CreateRealm() Realm {
+	return Realm{
+		identifier: ulid.MustNew(ulid.Now(), ulid.DefaultEntropy()),
+	}
+}
